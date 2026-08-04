f0c357628cd1bedfa8664f308250bf74e697fc42
Fabian Hernandez <53127498+fhernandezc184@users.noreply.github.com>
2026-08-04 13:29:36 +0200

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+
+        let indexBuyDay = 0
+        let globalProfit = 0
+
+        for(let i = 0; i < prices.length; i++){
+            
+            let currentProfit = prices[i] - prices[indexBuyDay]
+            console.log(currentProfit)
+            globalProfit = Math.max(globalProfit , currentProfit)
+
+            if(prices[indexBuyDay] > prices[i]){
+                indexBuyDay = i
+            }
+            
+        }
+
+
+
+        return globalProfit
+    }
+}
