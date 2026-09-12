557c0b4932ebe6059605a00526895d4c7dc10e3d
Fabian Hernandez <53127498+fhernandezc184@users.noreply.github.com>
2026-09-12 13:08:52 +0200

    Add: longest-consecutive-sequence - submission-13

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-13.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+
+        int answer = 0;
+        HashSet<int> seen = new HashSet<int>(nums);
+
+        foreach(int number in nums){
+            if(!seen.Contains(number-1)){
+                var count = 1;
+                while(seen.Contains(number + count)){
+                    count++;
+                }
+                if(count > answer) answer = count;
+            }
+        }
+        return answer;
+    }
+}
